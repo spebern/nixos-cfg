74ed2b51c391b7073ce71603d94a405a39a3a3a6
Bernhard Specht <bernhard.specht@pentlandfirth.com>
2022-10-15 11:35:49 +0200

    add i3 desktop

---
+++ modules/desktop/i3.nix
@@ -0,0 +1,61 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.i3;
+    configDir = config.dotfiles.configDir;
+in {
+  options.modules.desktop.i3 = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    modules.theme.onReload.i3 = ''
+      ${pkgs.i3}/bin/i3-msg reload
+      ${pkgs.i3}/bin/i3-msg restart
+    '';
+
+    environment.systemPackages = with pkgs; [
+      lightdm
+      dunst
+      libnotify
+      i3lock
+      (polybar.override {
+        pulseSupport = true;
+        nlSupport = true;
+      })
+    ];
+
+    services = {
+      picom.enable = true;
+      redshift.enable = true;
+      xserver = {
+        enable = true;
+        displayManager = {
+          defaultSession = "none+i3";
+          lightdm.enable = true;
+          lightdm.greeters.mini.enable = true;
+        };
+        windowManager.i3.enable = true;
+      };
+    };
+
+    systemd.user.services."dunst" = {
+      enable = true;
+      description = "";
+      wantedBy = [ "default.target" ];
+      serviceConfig.Restart = "always";
+      serviceConfig.RestartSec = 2;
+      serviceConfig.ExecStart = "${pkgs.dunst}/bin/dunst";
+    };
+
+    # link recursively so other modules can link files in their folders
+    home.configFile = {
+      "sxhkd".source = "${configDir}/sxhkd";
+      "i3" = {
+        source = "${configDir}/i3";
+        recursive = true;
+      };
+    };
+  };
+}
