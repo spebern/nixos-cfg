c32546918767f414a2abae9d1d3ffdc97278ed0f
Bernhard Specht <bernhard.specht@pentlandfirth.com>
2022-10-15 13:02:37 +0200

    change ssh pub key

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -16,8 +16,8 @@ in {
     };
 
     user.openssh.authorizedKeys.keys =
-      if config.user.name == "hlissner"
-      then [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB71rSnjuC06Qq3NLXQJwSz7jazoB+umydddrxL6vg1a hlissner" ]
+      if config.user.name == "bold"
+      then [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC5p8LEihMKI0nqasuMz9Ip92+8y/H6S3vGc13UpBosI bernhard@specht.net" ]
       else [];
   };
 }
