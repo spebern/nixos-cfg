b362ab73dda2795ae26517266f5bd4ef7b97644e
Bernhard Specht <bernhard.specht@pentlandfirth.com>
2022-10-15 12:21:23 +0200

    add wifi module with iwd

---
+++ modules/hardware/wifi.nix
@@ -0,0 +1,18 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.hardware.wifi;
+in
+{
+  options.modules.hardware.wifi = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    networking = {
+      networkmanager.wifi.backend = "iwd";
+      wireless.iwd.enable = true;
+    };
+  };
+}
