81557d7d53a0be04e2f7b7cc0b9c866092abb9a7
Bernhard Specht <bernhard@specht.net>
2022-10-30 14:03:57 +0100

    add go module

---
+++ modules/dev/go.nix
@@ -0,0 +1,29 @@
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let
+  devCfg = config.modules.dev;
+  cfg = devCfg.go;
+in
+{
+  options.modules.dev.go = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkMerge [
+    (mkIf cfg.enable {
+      user.packages = with pkgs; [
+        go
+        gotools
+        gopls
+        go-outline
+        gocode
+        gopkgs
+        gocode-gomod
+        godef
+        golint
+      ];
+    })
+  ];
+}
