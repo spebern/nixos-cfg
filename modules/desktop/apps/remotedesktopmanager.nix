ad8a9ada9d06c2d53ee46e5a195700de44cb0306
Bernhard Specht <bernhard@specht.net>
2022-11-06 16:40:44 +0100

    enable gnome spi2 core for remote desktop manager

--- modules/desktop/apps/remotedesktopmanager.nix
+++ modules/desktop/apps/remotedesktopmanager.nix
@@ -3,12 +3,14 @@
 with lib;
 with lib.my;
 let cfg = config.modules.desktop.apps.remotedesktopmanager;
-in {
+in
+{
   options.modules.desktop.apps.remotedesktopmanager = {
     enable = mkBoolOpt false;
   };
 
   config = mkIf cfg.enable {
+    services.gnome.at-spi2-core.enable = true;
     user.packages = with pkgs; [
       my.remotedesktopmanager
     ];
