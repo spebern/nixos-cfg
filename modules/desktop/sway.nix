345d155ea1ea18bb0e78a20fe9221386ddfe3e02
Bernhard Specht <bernhard@specht.net>
2022-12-27 11:02:41 +0100

    use vulkan renderer for sway

--- modules/desktop/sway.nix
+++ modules/desktop/sway.nix
@@ -270,6 +270,7 @@ in
           export XDG_SESSION_TYPE=wayland
           export XDG_SESSION_DESKTOP=sway
           export XDG_CURRENT_DESKTOP=sway
+          export WLR_RENDERER=vulkan
         '';
       };
     };
